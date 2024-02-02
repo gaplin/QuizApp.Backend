b29c7f69ba661d04138f539bc9b06081c1414393
gaplin <gaplinn@gmail.com>
2024-02-02 20:32:20 +0100

    Simplifed dbContext

--- QuizApp.Infrastructure/Context/QuizAppContext.cs
+++ QuizApp.Infrastructure/Context/QuizAppContext.cs
@@ -8,32 +8,16 @@ namespace QuizApp.Infrastructure.Context;
 
 internal class QuizAppContext : IQuizAppContext
 {
-    private readonly IMongoDatabase _db;
-    private readonly MongoDbSettings _dbSettings;
-    private IMongoCollection<QuizModel>? _quizzes;
-    private IMongoCollection<UserModel>? _users;
+    private readonly IMongoCollection<QuizModel> _quizzes;
+    private readonly IMongoCollection<UserModel> _users;
 
     public QuizAppContext(IMongoDatabase db, IOptions<MongoDbSettings> dbSettings)
     {
-        _db = db;
-        _dbSettings = dbSettings.Value;
+        _quizzes = db.GetCollection<QuizModel>(dbSettings.Value.Collections.Quizzes);
+        _users = db.GetCollection<UserModel>(dbSettings.Value.Collections.Users);
     }
 
-    IMongoCollection<QuizModel> IQuizAppContext.Quizzes
-    {
-        get
-        {
-            _quizzes ??= _db.GetCollection<QuizModel>(_dbSettings.Collections.Quizzes);
-            return _quizzes;
-        }
-    }
+    IMongoCollection<QuizModel> IQuizAppContext.Quizzes => _quizzes;
 
-    IMongoCollection<UserModel> IQuizAppContext.Users
-    {
-        get
-        {
-            _users ??= _db.GetCollection<UserModel>(_dbSettings.Collections.Users);
-            return _users;
-        }
-    }
+    IMongoCollection<UserModel> IQuizAppContext.Users => _users;
 }
