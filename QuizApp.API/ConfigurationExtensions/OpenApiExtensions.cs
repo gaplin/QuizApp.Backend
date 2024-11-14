3c489512a922bce9b6e77b38fe19f24a9171a4bf
gaplin <gaplinn@gmail.com>
2024-11-14 19:39:13 +0100

    Using new OpenApi lib

---
+++ QuizApp.API/ConfigurationExtensions/OpenApiExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace QuizApp.API.ConfigurationExtensions;
+
+public static class OpenApiExtensions
+{
+    public static OpenApiOptions UseJwtBearerAuthentication(this OpenApiOptions options)
+    {
+        var scheme = new OpenApiSecurityScheme()
+        {
+            Name = JwtBearerDefaults.AuthenticationScheme,
+            Type = SecuritySchemeType.Http,
+            Scheme = JwtBearerDefaults.AuthenticationScheme,
+            BearerFormat = "JWT",
+            Description = """
+                      JWT Authorization header using the Bearer scheme.<br>
+                      Enter your token in the text input below.<br>
+                      Example: '12345abcdef'
+                      """,
+            Reference = new()
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+            }
+        };
+        options.AddDocumentTransformer((doc, context, token) =>
+        {
+            doc.Components ??= new();
+            doc.Components.SecuritySchemes.Add(JwtBearerDefaults.AuthenticationScheme, scheme);
+            return Task.CompletedTask;
+        });
+        options.AddOperationTransformer((op, context, token) =>
+        {
+            if (context.Description.ActionDescriptor.EndpointMetadata.OfType<IAuthorizeData>().Any())
+            {
+                op.Security = [new() { [scheme] = [] }];
+            }
+            var req = new OpenApiSecurityRequirement();
+            return Task.CompletedTask;
+        });
+        return options;
+    }
+
+    public static OpenApiOptions TreatEnumAsStrings(this OpenApiOptions options)
+    {
+        options.AddSchemaTransformer((schema, context, token) =>
+        {
+            var type = context.JsonTypeInfo.Type;
+            if (type.IsEnum)
+            {
+                schema.Type = "string";
+            }
+            return Task.CompletedTask;
+        });
+
+        return options;
+    }
+}
--- QuizApp.API/Program.cs
+++ QuizApp.API/Program.cs
@@ -2,7 +2,7 @@ using Carter;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using QuizApp.API.ConfigurationExtensions;
 using QuizApp.Domain;
 using QuizApp.Infrastructure;
 using System.Security.Claims;
@@ -30,36 +30,10 @@ builder.Services
     .Configure<MvcJsonOptions>(options =>
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
     )
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen(c =>
+    .AddOpenApi(options =>
     {
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-        {
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer",
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = """
-                      JWT Authorization header using the Bearer scheme.<br>
-                      Enter 'Bearer' [space] and then your token in the text input below.<br>
-                      Example: 'Bearer 12345abcdef'
-                      """,
-        });
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                []
-            }
-        });
+        options.UseJwtBearerAuthentication();
+        options.TreatEnumAsStrings();
     })
     .AddMemoryCache()
     .AddInfrastructure(builder.Configuration.GetSection("QuizAppDatabase"))
@@ -113,9 +87,8 @@ app.UseCors();
 
 if (app.Environment.IsDevelopment())
 {
-    app
-        .UseSwagger()
-        .UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUI(opts => opts.SwaggerEndpoint("/openapi/v1.json", "QuizApp.API"));
 }
 app.UseAuthentication();
 app.UseAuthorization();
